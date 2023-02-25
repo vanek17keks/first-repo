41277b621e2a002a5542fcf1c7087624b54d5d59
vanek17keks <vanek.keks.official@gmail.com>
2023-02-25 20:39:59 +0300

    feat(HW3): add markup for light theme

---
+++ HW3/script.js
@@ -0,0 +1,53 @@
+initThemeSwitcher()
+
+function initThemeSwitcher() {
+    const themeHrefs = {
+        light: "day-variables.css",
+        dark: "night-variables.css",
+      },
+      head = document.getElementsByTagName("head")[0],
+      changeThemeButton = document.getElementById("change-theme-button");
+
+    let theme = window.matchMedia("(prefers-color-scheme: light)").matches
+      ? "light"
+      : "dark";
+
+    changeThemeButton.addEventListener("click", () => {
+      changeTheme(theme);
+    });
+
+    /**
+     * @param {'light' | 'dark'} currentTheme
+     */
+    function changeTheme(currentTheme) {
+      removeLinks();
+      const newTheme = currentTheme === "dark" ? "light" : "dark";
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      link.href = themeHrefs[newTheme];
+      head.appendChild(link);
+      setButtonText(theme);
+      theme = newTheme;
+    }
+
+    /**
+     * @param {'light' | 'dark'} currentTheme
+     */
+    function setButtonText(currentTheme) {
+      changeThemeButton.textContent = `${currentTheme} theme`;
+    }
+
+    function removeLinks() {
+      for (let link of head.getElementsByTagName("link")) {
+        for (const key in themeHrefs) {
+          if (link.href.includes(themeHrefs[key])) {
+              link.remove()
+          }
+        }
+      }
+    }
+
+    changeTheme(theme);
+    setButtonText(theme === "dark" ? "light" : "dark");
+}
